09afb503a2a7a4b2bd8eed6bf18b0355efd6f0bc
selenayack <89045431+selenayack@users.noreply.github.com>
2022-08-14 15:58:21 +0300

    added comments

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
 import numpy as np
 import cv2
 
+#resmi belirtilen konumdan okuduk
 img = cv2.imread('test2.jpg')
+#resim algılama işleminde kolaylık sağlaması için resmi gri yaptık
 grayImg= cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+#threshold eşik ayarlama maskesi için alt eşik ve üst eşiği belirledik
 lower = np.array([110, 110, 110])
 upper = np.array([255, 255, 255])
 
-
+#belirlenen degerlerle orjinal görsele threshold uygulandı
 threshed = cv2.inRange(img, lower, upper)
+
 
 cv2.imshow('Thresholded Image', threshed)
 
+#kenarları daha iyi algılamak için  threshold uygulanan görsele canny filtresi uygulandı
 canny=cv2.Canny(threshed,100,200)
 cv2.imshow('canny Image', canny)
 
+#canny filtresi uygulanmış görseldeki contour değerleri belirlendi
 contours , hierarchy = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
+# bulunan contour kısımları orjinal resimde çizildi
 cv2.drawContours(img, contours, -1, (0, 0, 100), 3)
 
 for c in contours:
-
+    #sınırlayıcı dörtgenin sol üst  x,y koordinatları ,genişlik ve yüksekliğine değişken atanır
     x, y , w, h = cv2.boundingRect(c)
+    #bulunan konturleri kapsayan dörtgenler  gri görselde çizilir
     grayImg = cv2.rectangle(grayImg,(x,y),(x+w,y+h),(0,255,0),2)
+
     cv2.imshow("Detected Shapes", grayImg)
     aspectRatio = float(w)/h
     area = cv2.contourArea(c)
